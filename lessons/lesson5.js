202f27d939d50a7de839b4329b326a325ab020cc
toooxa@gmail.com <toooxa@gmail.com>
2017-02-28 14:51:49 +0200

    dz

--- lessons/lesson5.js
+++ lessons/lesson5.js
@@ -1,49 +1,104 @@
-var charId = Math.floor(Math.random() * (100 - 1 + 1) + 1);
-console.log()
-
 var Entities = {
-    Player: function(data){
-        Entities.Base.call(this, data);
+    Player: function(data, name){
+        Entities.Base.apply(this, arguments);
         this.info = data.info;
         this.age = data.age;
         this.playRate = data.playRate;
         this.characters = [];
     },
     Character: function(data){
-        Entities.Base.call(this, data);
+        Entities.Base.apply(this, arguments);
         this.skills = data.skils;
         this.money = data.money;
         this.lvl = data.lvl;
     },
     Base: function(data){
-        this.id = charId;
+        this.id = Math.floor(Math.random() * (100 - 1 + 1) + 1);
         this.name = data.name;
         this.createdAt = new Date;
         this.updatedAt = new Date;
     }
 }
 
-var player = new Entities.Player({
-    name:"name",
-    info:"man",
-    age:20,
-    playRate:100
-})
-
-var char = new Entities.Character({
-    name:"name",
-    info:"man",
-    age:20,
-    playRate:100
-})
 
 Entities.Player.prototype = Object.create(Entities.Base.prototype)
 Entities.Player.constructor = Entities.Player;
 
+
+Entities.Player.prototype.addCharacter = function(character) {
+    if (!(character instanceof Entities.Character)) {
+        throw "Can't set character";
+    }
+    this.characters.push(character);
+}
+
+function getCharacter(charName){
+    var charList = player.characters;
+    for(key in charList){
+        if(charList[key].name == charName){
+            console.log(charList[key]);
+            break;
+        } else {
+            console.log(charName + ' не существует')
+        }
+    }
+}
+
+function getCharacters(){
+    console.log(player.characters);
+}
+
+function deleteCharacter(charName){
+    var charList = player.characters;
+    for(key in charList){
+        if(charList[key].name == charName){
+            console.log(charList[key].name + ' удален');
+            charList.splice(key, 1);
+            break;
+        } else {
+            console.log(charName + ' не существует')
+        }
+    }
+}
+
+
+//player.getCharacter(charName){
+//    var charList = this.characters;
+//    for(key in charList){
+//        if(charList[key].name == charName){
+//            console.log(charList[key]);
+//            break;
+//        } else {
+//            console.log(charName + ' не существует')
+//        }
+//    }
+//}
+
+
 Entities.Character.prototype = Object.create(Entities.Base.prototype)
 Entities.Character.constructor = Entities.Character;
 
-Entities.Player.prototype.addCharacter = function(character){
-    this.characters.push(character)
+
+var player = new Entities.Player({
+    name: 'player1',
+    info: 'ssdfsdfsdf',
+    age: 22,
+    playRate: 100
+});
+
+
+for (var i = 4; i >= 0; i--) {
+    player.addCharacter(new Entities.Character({
+        name: 'Character' + i,
+        skills: {
+            fire: 100,
+            stun: 10
+        },
+        money: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
+        lvl: Math.floor(Math.random() * (15 - 1 + 1)) + 1,
+    }))
 }
 
+
+
+console.log(player);
